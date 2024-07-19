a4477217e12f1159263c72b0b624eb7075ffd6fa
kedamarimo-yz <145884448+kedamarimo-yz@users.noreply.github.com>
2024-07-19 13:31:44 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+""" from fastapi import FastAPI
 from pydantic import BaseModel
 
 class Data(BaseModel):
@@ -14,4 +14,36 @@ def index():
 @app.post('/calc')
 def face_recognition(data: Data):
     z = data.x*data.y
-    return {'result': z}
+    return {'result': z} """
+    
+    
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security.api_key import APIKeyHeader
+from starlette.status import HTTP_403_FORBIDDEN
+import os
+from dotenv import load_dotenv
+
+# .envファイルから環境変数を読み込む（開発環境用）
+load_dotenv()
+
+# 環境変数からAPIキーを取得
+API_KEY = os.getenv("FACE_AUTH_KEY")
+API_KEY_NAME = "X-API-KEY"
+api_key_header = APIKeyHeader(name=API_KEY_NAME)
+
+app = FastAPI()
+
+def get_api_key(api_key: str = Depends(api_key_header)):
+    if api_key != API_KEY:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
+        )
+    return api_key
+
+@app.get("/")
+def read_root(api_key: str = Depends(get_api_key)):
+    return {"message": "Hello, World!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None, api_key: str = Depends(get_api_key)):
+    return {"item_id": item_id, "q": q}
